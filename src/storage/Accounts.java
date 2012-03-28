2ba8424a16be7b8e819a628b52768ae503898d21
Nick <t0pth4t@yahoo.com>
2012-03-28 17:56:44 -0500

    Changed a little

--- src/storage/Accounts.java
+++ src/storage/Accounts.java
@@ -32,6 +32,18 @@ public class Accounts implements Serializable {
 		}
 		return null;
 	}
+	public String getHash(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	       MessageDigest digest = MessageDigest.getInstance("SHA-512");
+	       digest.update(password.getBytes());
+	       byte[] hash = digest.digest();
+	       StringBuffer hexString = new StringBuffer();
+	    	for (int i=0;i<hash.length;i++) {
+	    		String hex=Integer.toHexString(0xff & hash[i]);
+	   	     	if(hex.length()==1) hexString.append('0');
+	   	     	hexString.append(hex);
+	    	}
+	    	return hexString.toString();
+	 }
 	
 	 
 }
