d23ef2ec40ea96dce285f1811826f4b1e6294fc0
Andrew Landpheir <landpheiran@gmail.com>
2012-05-07 15:17:12 -0500

    Added the radio buttons to the edit shell for user account edits

--- src/gui/view/EditShell.java
+++ src/gui/view/EditShell.java
@@ -30,6 +30,8 @@ import org.eclipse.swt.widgets.Text;
 //import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 
 public class EditShell {
@@ -289,7 +291,8 @@ public class EditShell {
 		Composite filterBtnComposite_NORTH = new Composite(filterBtnComposite, SWT.NONE);
 		formToolkit.adapt(filterBtnComposite_NORTH);
 		formToolkit.paintBordersFor(filterBtnComposite_NORTH);
-		filterBtnComposite_NORTH.setLayout(new FillLayout(SWT.HORIZONTAL));
+		filterBtnComposite_NORTH.setLayout(new GridLayout(1, false));
+		
 		
 		//Center Button Composite
 		Composite filterBtnComposite_CENTER = new Composite(filterBtnComposite, SWT.NONE);
@@ -548,7 +551,7 @@ public class EditShell {
 		
 		
 		// if editing user account
-		if (opened_user_account) {
+	if (opened_user_account) {
 			// add remove button to delete filters
 			Button btnREmove = new Button(filterBtnComposite_SOUTH, SWT.NONE);
 			GridData gd_btnREmove = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
@@ -583,6 +586,46 @@ public class EditShell {
 					}
 				}
 			});
+			
+			// ************************* YO YO YO YO ZONG ZONG ZONG ******************************
+			//TODO zong zong zong zong zong hey there good job with everything
+			//TODO ALMOST DONE
+			
+			// User group label in the north center panel
+			Label lblUserPermissions = new Label(filterBtnComposite_NORTH, SWT.NONE);
+			formToolkit.adapt(lblUserPermissions, true, true);
+			lblUserPermissions.setText("User Group:");
+			
+			final Button btnPower = new Button(filterBtnComposite_NORTH, SWT.RADIO);
+			formToolkit.adapt(btnPower, true, true);
+			btnPower.setText("Power");
+			
+			final Button btnStandard = new Button(filterBtnComposite_NORTH, SWT.RADIO);
+			formToolkit.adapt(btnStandard, true, true);
+			btnStandard.setText("Standard");
+			
+			if (accounts.getAccount(edit_account).getGroup() == Group.POWER) {btnPower.setSelection(true);}
+			if (accounts.getAccount(edit_account).getGroup() == Group.STANDARD) {btnStandard.setSelection(true);}
+
+			// power selection listener
+			btnPower.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					if (btnPower.getSelection() == true){
+						
+					}
+				}
+			});
+			
+			
+			// Standard seleciton listener
+			btnStandard.addSelectionListener(new SelectionAdapter(){
+				public void widgetSelected(SelectionEvent e){
+					if (btnStandard.getSelection() == true){
+						
+					}
+				}
+			});
+			
 		}
 				
 			
@@ -590,7 +633,7 @@ public class EditShell {
 				//Window was opened for editing user account/user group
 				//Add the extra check boxes for accounts and user groups
 				//Add reset password for accounts
-		} else {
+			} else {
 				// Import/Export Button
 				Button btnImport = new Button(btnBarComposite, SWT.NONE);
 				GridData gd_btnImport = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
@@ -776,6 +819,9 @@ public class EditShell {
 			btnCancel.setLayoutData(gd_btnCancel);
 			formToolkit.adapt(btnCancel, true, true);
 			btnCancel.setText("Cancel");
+			new Label(btnBarComposite, SWT.NONE);
+			new Label(btnBarComposite, SWT.NONE);
+			new Label(btnBarComposite, SWT.NONE);
 			new Label(btnBarComposite, SWT.NONE);
 			new Label(btnBarComposite, SWT.NONE);
 			new Label(btnBarComposite, SWT.NONE);
